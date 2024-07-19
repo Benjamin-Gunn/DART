5cc114c2ba7f3b4149ca46e76d2ae2b52f3ab23d
Benjamin-Gunn <benjamin.gunn.archive@gmail.com>
2024-07-18 20:44:10 -0600

    Fix "input.txt" boolean input

--- models/lorenz_96_tracer_advection/work/TESTS/test_run.sh
+++ models/lorenz_96_tracer_advection/work/TESTS/test_run.sh
@@ -19,14 +19,12 @@ setup_test () {
 	# Set up input.nml to do the initial perfect_model_obs run
 	cp TESTS/TEST_BASE_INPUT.nml input.nml
 
-	vi input.nml << HERE
-:1,\$s/T_QCEFF_TABLE_FILENAME/$1/
-:1,\$s/T_READ_INPUT_STATE_FROM_FILE/.false./
-:1,\$s/T_POST_INF_FLAVOR/$6/
-:1,\$s/T_POSITIVE_TRACER/$4/
-:1,\$s/T_BOUNDED_ABOVE_IS_ONE/$5/
-:wq
-HERE
+	sed -i "s/T_QCEFF_TABLE_FILENAME/$1/g" input.nml
+	sed -i "s/T_READ_INPUT_STATE_FROM_FILE/.false./g" input.nml
+	sed -i "s/T_POST_INF_FLAVOR/$6/g" input.nml
+	sed -i "s/T_POSITIVE_TRACER/$4/g" input.nml
+	sed -i "s/T_BOUNDED_ABOVE_IS_ONE/$5/g" input.nml
+	
    	./perfect_model_obs
 
    	# Do the next perfect_model iteration and the filter
@@ -34,17 +32,15 @@ HERE
 
    	# Do ensemble size of 160 so that subsequent ICs with mpirun will have same ICs
 	cp TESTS/TEST_BASE_INPUT.nml input.nml
-   	vi input.nml << HERE
-:1,\$s/T_QCEFF_TABLE_FILENAME/$1/
-:1,\$s/T_READ_INPUT_STATE_FROM_FILE/.true./
-:1,\$s/T_FILTER_INPUT/perfect_input.nc/
-:1,\$s/T_ENS_SIZE/160/
-:1,\$s/T_POST_INF_FLAVOR/$6/
-:1,\$s/T_PERTURB_FROM_SINGLE_INSTANCE/.true./
-:1,\$s/T_POSITIVE_TRACER/$4/
-:1,\$s/T_BOUNDED_ABOVE_IS_ONE/$5/
-:wq
-HERE
+
+	sed -i "s/T_QCEFF_TABLE_FILENAME/$1/g" input.nml
+	sed -i "s/T_READ_INPUT_STATE_FROM_FILE/.true./g" input.nml
+	sed -i "s/T_FILTER_INPUT/perfect_input.nc/" input.nml
+	sed -i "s/T_ENS_SIZE/160/g" input.nml
+	sed -i "s/T_POST_INF_FLAVOR/$6/g" input.nml
+	sed -i "s/T_PERTURB_FROM_SINGLE_INSTANCE/.true./g" input.nml
+	sed -i "s/T_POSITIVE_TRACER/$4/g" input.nml
+	sed -i "s/T_BOUNDED_ABOVE_IS_ONE/$5/g" input.nml
 
    	./perfect_model_obs
    	./filter
@@ -58,17 +54,16 @@ HERE
 
 range_test () {
 	cp TESTS/TEST_BASE_INPUT.nml input.nml
-	vi input.nml << HERE
-:1,\$s/T_QCEFF_TABLE_FILENAME/$1/
-:1,\$s/T_READ_INPUT_STATE_FROM_FILE/.true./
-:1,\$s/T_FILTER_INPUT/filter_input.nc/
-:1,\$s/T_ENS_SIZE/$2/
-:1,\$s/T_PERTURB_FROM_SINGLE_INSTANCE/.false./
-:1,\$s/T_POST_INF_FLAVOR/$6/
-:1,\$s/T_POSITIVE_TRACER/$4/
-:1,\$s/T_BOUNDED_ABOVE_IS_ONE/$5/
-:wq
-HERE
+
+        sed -i "s/T_QCEFF_TABLE_FILENAME/$1/g" input.nml
+        sed -i "s/T_READ_INPUT_STATE_FROM_FILE/.true./g" input.nml
+        sed -i "s/T_FILTER_INPUT/filter_input.nc/g" input.nml
+        sed -i "s/T_ENS_SIZE/$2/g" input.nml
+        sed -i "s/T_PERTURB_FROM_SINGLE_INSTANCE/.false./g" input.nml
+        sed -i "s/T_POST_INF_FLAVOR/$6/g" input.nml
+        sed -i "s/T_POSITIVE_TRACER/$4/g" input.nml
+        sed -i "s/T_BOUNDED_ABOVE_IS_ONE/$5/g" input.nml
+
 	# Make sure there is no file around in case filter fails
 	rm filter_output.nc
 
@@ -81,7 +76,7 @@ HERE
 }
 
 # Tests called down here so that functions are declared,
-# Also, the script is currently in the work, so no need to cd
+# Also, the script should be currently in the test work directory
 
 if [[ $3 -eq 0 ]]; then
 	setup_test $1 $2 $3 $4 $5 $6
