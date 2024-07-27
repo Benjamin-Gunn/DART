14d448721cab5738ae6d6d8a76a250283049b497
Benjamin-Gunn <benjamin.gunn.archive@gmail.com>
2024-07-26 21:22:41 -0600

    Fix clean-up to operate in developer_test

--- developer_tests/tracer_filter/test_cleanup.sh
+++ developer_tests/tracer_filter/test_cleanup.sh
@@ -3,6 +3,6 @@
 # $1 = number of job arrays tested
 
 for (( i=0 ; i<$1 ; i++ )); do
-	cat ../../work_test_$i/temp_test_output >> ../../../developer_tests/tracer_filter/test_output
-	rm -r ../../work_test_$i
+	cat ~/DART/models/lorenz_96_tracer_advection/work_test_$i/temp_test_output >> ~/DART/developer_tests/tracer_filter/test_output
+	rm -r ~/DART/models/lorenz_96_tracer_advection/work_test_$i
 done
